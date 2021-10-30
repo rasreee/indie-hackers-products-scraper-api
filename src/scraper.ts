18d2be838f513217d82edbc555b547abb2301b50
Lesley Chang <lesley.hanna98@gmail.com>
2021-10-30 16:21:00 -0400

    FINALLY

--- src/scraper.ts
+++ src/scraper.ts
@@ -1,14 +1,44 @@
-import axios from 'axios';
-import { logger } from '@utils/logger';
+import puppeteer from 'puppeteer';
+import fs from 'fs';
+import { logger } from './utils/logger';
+
+const scraperApiKey = process.env.SCRAPER_API_KEY;
+
+const productsUrl = 'https://indiehackers.com/products';
+const proxyUrl = `http://scraperapi.render=true:${scraperApiKey}@proxy-server.scraperapi.com:8001`;
+
+const getVisual = async () => {
+  try {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.goto(productsUrl);
+    await page.waitForSelector('.product-card__link');
+
+    const result = await page.evaluate(() => {
+      const linkEls = document.querySelectorAll('a.product-card__link');
+      const links: string[] = [];
+      for (let i = 0; i < linkEls.length; i++) {
+        links[i] = linkEls[i].getAttribute('href');
+      }
+
+      return links;
+    });
+    await browser.close();
+    return result;
+  } catch (error) {
+    console.error(error);
+  }
+};
 
 class Scraper {
-  public url = 'https://www.indiehackers.com/products';
-
   public async init() {
-    const response = await axios.get(this.url);
-
-    logger.info('👌 Scraped page data: ');
-    logger.info(response.data);
+    const page = await getVisual();
+    fs.writeFile('products.json', JSON.stringify(page), err => {
+      if (err) throw err;
+      console.log('✅ SUCCESS!');
+    });
+    logger.info('\n⭐ RESULT:\n');
+    logger.info(page);
   }
 }
 
