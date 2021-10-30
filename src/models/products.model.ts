e1db3c26e2ec40a4eea5dda251ae5f55538d59f9
Lesley Chang <lesley.hanna98@gmail.com>
2021-10-30 18:02:09 -0400

    fix type errors

--- src/models/products.model.ts
+++ src/models/products.model.ts
@@ -2,14 +2,14 @@ import { Product, RevenueExplanation } from '@interfaces/products.interface';
 
 const productModel: Product[] = [
   {
-    id: 1,
+    id: 'airgram',
     name: 'Airgram',
     tagline: 'Recording, transcript, notes for zoom & google meet.',
     revenueExplanation: RevenueExplanation.SelfReported,
     monthlyRevenue: 0,
   },
   {
-    id: 1,
+    id: 'karma',
     name: 'Karma',
     tagline: 'WFH sorted: praise, rewards, surveys and watercooler chats',
     revenueExplanation: RevenueExplanation.StripeVerified,
