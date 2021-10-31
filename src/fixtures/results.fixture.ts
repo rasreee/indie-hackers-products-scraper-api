5765d2453869049a2381735adad8e4bf88fa0af8
Lesley Chang <lesley.hanna98@gmail.com>
2021-10-31 03:01:36 -0400

    finish mapping types

---
+++ src/fixtures/results.fixture.ts
@@ -0,0 +1,117 @@
+export const productTags = [
+  'commitment-full-time',
+  'commitment-side-project',
+  'employees-0',
+  'employees-10-plus',
+  'employees-under-10',
+  'founders-code-no',
+  'founders-code-yes',
+  'founders-solo',
+  'funding-bootstrapped',
+  'funding-seed',
+  'funding-self',
+  'platform-browser',
+  'platform-mobile',
+  'platform-web',
+  'revenue-model-commission',
+  'revenue-model-free',
+  'revenue-model-partnerships',
+  'revenue-model-subscription',
+  'revenue-model-transactional',
+  'stripe-verified-revenue',
+  'vertical-ai',
+  'vertical-analytics',
+  'vertical-apis',
+  'vertical-b2b',
+  'vertical-b2c',
+  'vertical-bots',
+  'vertical-calendar',
+  'vertical-games',
+  'vertical-communication',
+  'vertical-community',
+  'vertical-content',
+  'vertical-email-marketing',
+  'vertical-finance',
+  'vertical-health-fitness',
+  'vertical-marketing',
+  'vertical-productivity',
+  'vertical-programming',
+  'vertical-saas',
+  'vertical-sales',
+  'vertical-shopping',
+  'vertical-social-media',
+  'vertical-task-management',
+  'vertical-writing',
+] as const;
+
+export type ProductTag = typeof productTags[number];
+
+export enum MatchLevel {
+  none = 'none',
+}
+
+export type HighlightResult = {
+  value: string;
+  matchLevel: MatchLevel;
+  matchedWords: string[];
+};
+
+export type GetProductsData = {
+  results: {
+    hits: GetProductHit[];
+    nbHits: number;
+    page: number;
+    nbPages: number;
+    hitsPerPage: number;
+    exhaustiveNbHits: boolean;
+    exhaustiveTypo: boolean;
+    query: string;
+    params: string;
+    index: 'products';
+    processingTimeMS: number;
+  }[];
+};
+
+export enum HighlightKey {
+  name = 'name',
+  tagline = 'tagline',
+  twitterHandle = 'twitterHandle',
+  websiteUrl = 'websiteUrl',
+}
+
+export type GetProductHit = {
+  _tags: ProductTag[];
+  approvedTimestamp: number;
+  avatarUrl: string;
+  bumpedTimestamp: number;
+  createdTimestamp: number;
+  description: string;
+  last30DaysUniques: null;
+  name: string;
+  numFollowers: number;
+  productId: string;
+  publishedTimestamp: number;
+  revenue: number;
+  startDateStr: string;
+  tagline: string;
+  twitterHandle: string;
+  updatedTimestamp: number;
+  userIds: string[];
+  websiteUrl: string;
+  objectID: string;
+  _highlightResult: Record<HighlightKey, HighlightResult>;
+};
+
+export type GetProductsResults = {
+  hits: GetProductHit[];
+  nbHits: number;
+  page: number;
+  nbPages: number;
+  hitsPerPage: number;
+  exhaustiveNbHits: boolean;
+  exhaustiveTypo: boolean;
+  query: string;
+  params: string;
+  index: string;
+  processingTimeMS: number;
+};
--- src/services/indie-hackers.service.ts
+++ src/services/indie-hackers.service.ts
@@ -1,3 +1,4 @@
+import { GetProductHit, GetProductsResults } from '@fixtures/results.fixture';
 import axios from 'axios';
 
 const API_URL =
@@ -5,53 +6,35 @@ const API_URL =
 
 class IndieHackersService {
   getProducts = async () => {
-    // const response = await fetch(
-    //   'https://n86t1r3owz-dsn.algolia.net/1/indexes/*/queries?x-algolia-agent=Algolia%20for%20JavaScript%20(3.35.1)%3B%20Browser%20(lite)%3B%20JS%20Helper%202.21.1&x-algolia-application-id=N86T1R3OWZ&x-algolia-api-key=5140dac5e87f47346abbda1a34ee70c3',
-    //   {
-    // headers: {
-    //   accept: 'application/json',
-    //   'accept-language': 'en-US,en;q=0.9',
-    //   'cache-control': 'no-cache',
-    //   'content-type': 'application/x-www-form-urlencoded',
-    //   pragma: 'no-cache',
-    //   'sec-ch-ua': '"Google Chrome";v="95", "Chromium";v="95", ";Not A Brand";v="99"',
-    //   'sec-ch-ua-mobile': '?0',
-    //   'sec-ch-ua-platform': '"macOS"',
-    //   'sec-fetch-dest': 'empty',
-    //   'sec-fetch-mode': 'cors',
-    //   'sec-fetch-site': 'cross-site',
-    //   Referer: 'https://www.indiehackers.com/products',
-    //   'Referrer-Policy': 'unsafe-url',
-    // },
-    // body: '{"requests":[{"indexName":"products","params":"query=&hitsPerPage=16&page=0&restrictSearchableAttributes=&facets=%5B%5D&tagFilters="}]}',
-    //     method: 'POST',
-    //   },
-    // );
-
     const body = {
       requests: [{ indexName: 'products', params: 'query=&hitsPerPage=16&page=0&restrictSearchableAttributes=&facets=%5B%5D&tagFilters=' }],
     };
 
+    const headers = {
+      accept: 'application/json',
+      'accept-language': 'en-US,en;q=0.9',
+      'cache-control': 'no-cache',
+      'content-type': 'application/x-www-form-urlencoded',
+      pragma: 'no-cache',
+      'sec-ch-ua': '"Google Chrome";v="95", "Chromium";v="95", ";Not A Brand";v="99"',
+      'sec-ch-ua-mobile': '?0',
+      'sec-ch-ua-platform': '"macOS"',
+      'sec-fetch-dest': 'empty',
+      'sec-fetch-mode': 'cors',
+      'sec-fetch-site': 'cross-site',
+      Referer: 'https://www.indiehackers.com/products',
+      'Referrer-Policy': 'unsafe-url',
+    };
+
     const response = await axios.post(API_URL, body, {
-      headers: {
-        accept: 'application/json',
-        'accept-language': 'en-US,en;q=0.9',
-        'cache-control': 'no-cache',
-        'content-type': 'application/x-www-form-urlencoded',
-        pragma: 'no-cache',
-        'sec-ch-ua': '"Google Chrome";v="95", "Chromium";v="95", ";Not A Brand";v="99"',
-        'sec-ch-ua-mobile': '?0',
-        'sec-ch-ua-platform': '"macOS"',
-        'sec-fetch-dest': 'empty',
-        'sec-fetch-mode': 'cors',
-        'sec-fetch-site': 'cross-site',
-        Referer: 'https://www.indiehackers.com/products',
-        'Referrer-Policy': 'unsafe-url',
-      },
+      headers,
     });
+
     if (response.status === 200) {
-      return response.data;
+      const results: GetProductsResults = response.data.results;
+      return results;
     }
+
     const error = new Error(response.statusText);
     throw error;
   };
